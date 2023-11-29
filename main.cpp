bd6ad926fee281c95e01e1749c969329b9e26bba
Hend Ahmed <92755637+LifelongLearner-HEND@users.noreply.github.com>
2023-11-29 23:38:14 +0200

    Binary search for book by ISBN

--- main.cpp
+++ main.cpp
@@ -9,69 +9,37 @@ using namespace std;
 //For Linked List -> LLAuthor ->(#, ID(for author), Pointer)
 //                -> LLBook   ->(#, ID(for book), Pointer)
 
-
-//struct Author {
-//    char authorID[15];
-//    char authorName[30];
-//    char address[30];
-//};
-//
-//struct Book {
-//    char ISBN[15];
-//    char bookTitle[30];
-//    char authorID[30];
-//};
-//
-//struct ReferenceNode {
-//    int idPointer;
-//    int nextReferenceOffset;
-//};
-//
-//struct SecondaryIndexNode {
-//    char name[30];
-//    int referenceListOffset;
-//};
-
-// Function declarations
-
-//bool doesIDExist(fstream& primary, int x);
-//void insertAuthorPrimary(char id[], short offset);
-//void insertBookPrimary(char id[], short offset);
-//void insertAuthorName(char name[30], char ID[15]);
-//void sortSecondaryIndex(const char* filename);
-//void insertAuthorName(char name[30], char ID[15]);
-void getAuthorByID(int id, fstream &inFile);
+bool getAuthorByID(int id, fstream &indexFile); // search for the author by id and print the author record
+bool getBookByISBN(int isbn, fstream &indexFile); // search for the book by isbn and print the book record
 
 
 int main() {
-    fstream primary("PrimaryIndexAuthor.txt", ios::in);
-    int n; cin >> n;
-    getAuthorByID(n, primary);
-    primary.close();
 
+    fstream primary("PrimaryIndexBook.txt", ios::in);
+    int n; cin >> n;
+    getBookByISBN(n, primary);
+    primary.close();
     return 0;
 }
-// a function that takes the authorID as a parameter
-// and binary search for the authorID in the PrimaryIndexAuthor file
-// and get the offset of the author record
-// then seek to the offset in the Author file and read the author record
-void getAuthorByID(int id, fstream &inFile) {
+// A function that takes the authorID as a parameter and binary search for the authorID in the PrimaryIndexAuthor file
+// and get the offset of the author record then seek to the offset in the Author file and read the author record
+bool getAuthorByID(int id, fstream &indexFile) {
         // open the PrimaryIndexAuthor file in the read mode
         // count the number of records in the PrimaryIndexAuthor file
         int count = 0;
         string line;
-        while (getline(inFile, line))
+        while (getline(indexFile, line))
         {
             count++;
         }
         // loop through the PrimaryIndexAuthor file
         // and get each id and compare it with the id parameter
-        inFile.clear();
-        inFile.seekg(0, ios::beg);
+        indexFile.clear();
+        indexFile.seekg(0, ios::beg);
         // store records into a map to sort them
         vector<pair<int, int>> records;
         for(int i = 0; i < count; i++) {
-            if(getline(inFile, line)) {
+            if(getline(indexFile, line)) {
                 int currentID, currentOffset;
                 // before the space is the id
                 // after the space is the offset
@@ -81,7 +49,7 @@ void getAuthorByID(int id, fstream &inFile) {
                 records.emplace_back(currentID, currentOffset);
             }
         }
-        inFile.close();
+        indexFile.close();
 
         // binary search for the id
         int left = 0, right = count-1, mid;
@@ -129,6 +97,87 @@ void getAuthorByID(int id, fstream &inFile) {
             cout << "ID: " << ID << "\nName: "<< name << "\nAddress: " << address << endl;
         }
         else {
-            cout << "Author is not found!" << endl;
+            cout << "Author is NOT found!" << endl;
         }
+    return found;
+}
+
+// A function that takes the ISBN as a parameter and binary search for the ISBN in the PrimaryIndexBook file
+bool getBookByISBN(int isbn, fstream &indexFile) {
+    // open the PrimaryIndexAuthor file in the read mode
+    // count the number of records in the PrimaryIndexAuthor file
+    int count = 0;
+    string line;
+    while (getline(indexFile, line))
+    {
+        count++;
+    }
+    // loop through the PrimaryIndexAuthor file
+    // and get each id and compare it with the id parameter
+    indexFile.clear();
+    indexFile.seekg(0, ios::beg);
+    // store records into a map to sort them
+    vector<pair<int, int>> records;
+    for(int i = 0; i < count; i++) {
+        if(getline(indexFile, line)) {
+            int currentISBN, currentOffset;
+            // before the space is the id
+            // after the space is the offset
+            currentISBN = stoi(line.substr(0, line.find(' ')));
+            currentOffset = stoi(line.substr(line.find(' ') + 1));
+            // insert the record into the map
+            records.emplace_back(currentISBN, currentOffset);
+        }
+    }
+    indexFile.close();
+
+    // binary search for the id
+    int left = 0, right = count-1, mid;
+    int offset;
+    bool found = false;
+    while(left <= right) {
+        mid = left + (right - left) / 2;
+        if(records[mid].first == isbn) {
+            found = true;
+            offset = records[mid].second;
+            break;
+        }
+        else if (records[mid].first < isbn)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    if(found) {
+        cout << "Book is found!" << endl;
+        // open the Author file in the read mode
+        ifstream book("Book.txt", ios:: binary);
+        // seek to the offset
+        book.seekg(offset, ios::beg);
+        // read the length of the author record
+        char length[3];
+        length[2] = '\0';
+        book.read(length, 2);
+        // convert the length to integer
+        int len = stoi(length);
+        // read about len bytes from the current position
+        char* record = new char[len+1];
+        record[len] = '\0';
+        book.read(record, len);
+        book.close();
+        // parse the record
+        stringstream ss(record);
+        string ISBN, title, authorID;
+        getline(ss, ISBN, '|');
+        getline(ss, title, '|');
+        getline(ss, authorID, '|');
+        cout << "ID: " << ISBN << "\nName: " << title << "\nAddress: " << authorID << endl;
+    }
+    else {
+        cout << "Book is NOT found!" << endl;
+    }
+    return found;
 }
